4687855981be87e8be798d7229f78b21a0eb75de
Renan Rondon <renanrondon@live.com>
2022-10-04 22:22:01 -0300

    Adicionei as informações de Renan Rondon

---
+++ users/Rrhapsod.js
@@ -0,0 +1,138 @@
+export const data = {
+  // LEIA OS COMENTÁRIOS COM ATENÇÃO.
+  // QUALQUER DÚVIDA ENTRE EM CONTATO COM [Valdemir Filho] NO DISCORD DO EXPLORER
+
+  // Preencha os campos a seguir.
+  // Os valores devem estar dentro das aspas duplas (Ex: "Aqui dentro")
+
+  // Preencha com o seu nome.
+  name: "Renan Rondon",
+
+  // Preencha com o seu e-mail.
+  email: "renanrondon@live.com",
+
+  // Preencha com a cidade onde você mora.
+  city: "Araçatuba",
+
+  // Preencha com a sigla do estado onde você mora.
+  // Ex: SP
+  state: "SP",
+
+  // Preencha com o país onde mora.
+  country: "Brasil",
+
+  // Preencha de qual turma do Explorer você é: Ex (turma: "01").
+  // Se você for instrutor da Rocketseat coloque "00".
+  turma: "02",
+
+  // Preencha com a url do seu perfil no github: (https://github.com/[user]).
+  github: "https://github.com/Rrhapsod",
+
+  // Preencha com a url do seu perfil no linkedin: (https://www.linkedin.com/in/[user]).
+  linkedin_url: "https://www.linkedin.com/in/renan-rondon-748093226/",
+
+  // Preencha com a url do seu perfil da rockeseat: (https://app.rocketseat.com.br/me/[user]).
+  rocketseat_profile: "https://app.rocketseat.com.br/me/renan-de-lima-rondon-05779",
+
+  // Preencha os campos abaixo com os links dos desafios que você já concluiu.
+  // Preencha o campo [codigo_url] com o link do codepen ou github.
+  // Preencha o campo [deploy_url] caso tenha feito o deploy do seu projeto: Ex: Github Pages, Netlify, Vercel.
+
+  // Desafios do Stage 02
+  stage02: {
+    fase01_corrigindo_bugs01: {
+      codigo_url: "https://codepen.io/rrhapsody/pen/OJQgPxY",
+      deploy_url: ""
+    },
+    fase02_corrigindo_bugs02: {
+      codigo_url: "https://codepen.io/rrhapsody/pen/WNMObPw",
+      deploy_url: ""
+    },
+    fase02_semantica_acessibilidade: {
+      codigo_url: "https://codepen.io/rrhapsody/pen/NWygPZN",
+      deploy_url: ""
+    },
+    fase02_recriando_layout: {
+      codigo_url: "https://codepen.io/rrhapsody/pen/ExQGRvg",
+      deploy_url: ""
+    }
+  },
+
+  // Desafios do Stage 03
+  stage03: {
+    criando_formularios: {
+      codigo_url: "https://codepen.io/rrhapsody/pen/bGLzqGB",
+      deploy_url: ""
+    },
+    mobile_first: {
+      codigo_url: "https://codepen.io/rrhapsody/pen/ExQBROr",
+      deploy_url: ""
+    },
+    grid_com_animacoes: {
+      codigo_url: "https://codepen.io/rrhapsody/pen/ExQBROr",
+      deploy_url: ""
+    }
+  },
+
+  // Desafios do Stage 04
+  stage04: {
+    exercicio01: {
+      codigo_url: "https://codepen.io/rrhapsody/pen/QWmWLvb",
+      deploy_url: ""
+    },
+    exercicio02: {
+      codigo_url: "https://codepen.io/rrhapsody/pen/YzazaZm",
+      deploy_url: ""
+    }
+  },
+
+  // Desafios do Stage 05
+  stage05: {
+    focustimer: {
+      codigo_url: "https://github.com/Rrhapsod/FocusTimer",
+      deploy_url: "https://rrhapsod.github.io/FocusTimer/"
+    },
+    focustimer_dark_mode: {
+      codigo_url: "https://github.com/Rrhapsod/FocusTimer",
+      deploy_url: "https://rrhapsod.github.io/FocusTimer/"
+    }
+  },
+
+  // Desafios do Stage 06
+  stage06: {
+    spa_universe: {
+      codigo_url: "https://github.com/Rrhapsod/SPAUniverse",
+      deploy_url: "https://rrhapsod.github.io/SPAUniverse/"
+    },
+    gitfav: {
+      codigo_url: "https://github.com/Rrhapsod/PokeFav",
+      deploy_url: "https://rrhapsod.github.io/PokeFav/"
+    }
+  },
+
+
+  // Desafios do Stage 07
+  stage07: {
+    git_push: {
+      codigo_url: "https://github.com/Rrhapsod?tab=repositories"
+    }
+  },
+
+  // O projeto Rocket Movies será desenvolvido nos Stages 08, 09, 10, 11
+  stages_08_09_10_11: {
+    rocket_movies: {
+      github_frontend: "",
+      github_backend: "",
+      deploy_url: ""
+    }
+  },
+
+  // Projeto final
+  desafio_final: {
+    food_explorer: {
+      github_frontend: "",
+      github_backend: "",
+      deploy_url: ""
+    }
+  }
+}
