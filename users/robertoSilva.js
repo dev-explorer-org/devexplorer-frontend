1e2d369b2d57bdb5a02de8e1bb9a1ffdd53cba21
Renan Rondon <renanrondon@live.com>
2022-10-08 11:38:51 -0300

    Arrumando bandeira do Alagoas(Roberto Silva)

--- users/robertoSilva.js
+++ users/robertoSilva.js
@@ -6,33 +6,33 @@ export const data = {
   // Os valores devem estar dentro das aspas duplas (Ex: "Aqui dentro")
 
   // Preencha com o seu nome.
-  name: "Roberto Silva",
+  name: 'Roberto Silva',
 
   // Preencha com o seu e-mail.
-  email: "robertosilva.2@hotmail.com",
+  email: 'robertosilva.2@hotmail.com',
 
   // Preencha com a cidade onde você mora.
-  city: "Maceió",
+  city: 'Maceió',
 
   // Preencha com a sigla do estado onde você mora.
   // Ex: SP
-  state: "Al",
+  state: 'AL',
 
   // Preencha com o país onde mora.
-  country: "Brasil",
+  country: 'Brasil',
 
   // Preencha de qual turma do Explorer você é: Ex (turma: "01").
   // Se você for instrutor da Rocketseat coloque "00".
-  turma: "01",
+  turma: '01',
 
   // Preencha com a url do seu perfil no github: (https://github.com/[user]).
-  github: "https://github.com/Roberto-frontend-developer",
+  github: 'https://github.com/Roberto-frontend-developer',
 
   // Preencha com a url do seu perfil no linkedin: (https://www.linkedin.com/in/[user]).
-  linkedin_url: "https://www.linkedin.com/in/roberto-silva-58965b15b/",
+  linkedin_url: 'https://www.linkedin.com/in/roberto-silva-58965b15b/',
 
   // Preencha com a url do seu perfil da rockeseat: (https://app.rocketseat.com.br/me/[user]).
-  rocketseat_profile: "https://app.rocketseat.com.br/me/jose-roberto-da-silva-santos-1568832812",
+  rocketseat_profile: 'https://app.rocketseat.com.br/me/jose-roberto-da-silva-santos-1568832812',
 
   // Preencha os campos abaixo com os links dos desafios que você já concluiu.
   // Preencha o campo [codigo_url] com o link do codepen ou github.
@@ -41,98 +41,97 @@ export const data = {
   // Desafios do Stage 02
   stage02: {
     fase01_corrigindo_bugs01: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     fase02_corrigindo_bugs02: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     fase02_semantica_acessibilidade: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     fase02_recriando_layout: {
-      codigo_url: "",
-      deploy_url: ""
-    }
+      codigo_url: '',
+      deploy_url: '',
+    },
   },
 
   // Desafios do Stage 03
   stage03: {
     criando_formularios: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     mobile_first: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     grid_com_animacoes: {
-      codigo_url: "",
-      deploy_url: ""
-    }
+      codigo_url: '',
+      deploy_url: '',
+    },
   },
 
   // Desafios do Stage 04
   stage04: {
     exercicio01: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     exercicio02: {
-      codigo_url: "",
-      deploy_url: ""
-    }
+      codigo_url: '',
+      deploy_url: '',
+    },
   },
 
   // Desafios do Stage 05
   stage05: {
     focustimer: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     focustimer_dark_mode: {
-      codigo_url: "",
-      deploy_url: ""
-    }
+      codigo_url: '',
+      deploy_url: '',
+    },
   },
 
   // Desafios do Stage 06
   stage06: {
     spa_universe: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     gitfav: {
-      codigo_url: "",
-      deploy_url: ""
-    }
+      codigo_url: '',
+      deploy_url: '',
+    },
   },
-
 
   // Desafios do Stage 07
   stage07: {
     git_push: {
-      codigo_url: ""
-    }
+      codigo_url: '',
+    },
   },
 
   // O projeto Rocket Movies será desenvolvido nos Stages 08, 09, 10, 11
   stages_08_09_10_11: {
     rocket_movies: {
-      github_frontend: "",
-      github_backend: "",
-      deploy_url: ""
-    }
+      github_frontend: '',
+      github_backend: '',
+      deploy_url: '',
+    },
   },
 
   // Projeto final
   desafio_final: {
     food_explorer: {
-      github_frontend: "",
-      github_backend: "",
-      deploy_url: ""
-    }
-  }
-}
+      github_frontend: '',
+      github_backend: '',
+      deploy_url: '',
+    },
+  },
+};
