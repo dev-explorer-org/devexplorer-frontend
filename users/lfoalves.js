4555fac6e23afe11f4905f195c9fbc19f91e191c
Valdemir Filho <filhux@gmail.com>
2022-10-09 12:09:31 -0300

    fix

--- users/lfoalves.js
+++ users/lfoalves.js
@@ -6,60 +6,33 @@ export const data = {
   // Os valores devem estar dentro das aspas duplas (Ex: "Aqui dentro")
 
   // Preencha com o seu nome.
-<<<<<<<< HEAD:users/lfoalves.js
-  name: "Luiz Fernando",
+  name: 'Luiz Fernando',
 
   // Preencha com o seu e-mail.
-  email: "lfoalves.web@gmail.com",
+  email: 'lfoalves.web@gmail.com',
 
   // Preencha com a cidade onde você mora.
-  city: "Boa Vista",
+  city: 'Boa Vista',
 
   // Preencha com a sigla do estado onde você mora.
   // Ex: SP
-  state: "RR",
-========
-  name: "Marcus Vinícius",
-
-  // Preencha com o seu e-mail.
-  email: "marcusv892@gmail.com",
-
-  // Preencha com a cidade onde você mora.
-  city: "Rio Maria",
-
-  // Preencha com a sigla do estado onde você mora.
-  // Ex: SP
-  state: "PA",
->>>>>>>> viniseven-viniseven:users/viniseven.js
+  state: 'RR',
 
   // Preencha com o país onde mora.
-  country: "Brasil",
+  country: 'Brasil',
 
   // Preencha de qual turma do Explorer você é: Ex (turma: "01").
   // Se você for instrutor da Rocketseat coloque "00".
-<<<<<<<< HEAD:users/lfoalves.js
-  turma: "04",
+  turma: '04',
 
   // Preencha com a url do seu perfil no github: (https://github.com/[user]).
-  github: "https://github.com/lfoalves",
+  github: 'https://github.com/lfoalves',
 
   // Preencha com a url do seu perfil no linkedin: (https://www.linkedin.com/in/[user]).
-  linkedin_url: "https://www.linkedin.com/in/lfoalves",
+  linkedin_url: 'https://www.linkedin.com/in/lfoalves',
 
   // Preencha com a url do seu perfil da rockeseat: (https://app.rocketseat.com.br/me/[user]).
-  rocketseat_profile: "https://app.rocketseat.com.br/me/lfoalves",
-========
-  turma: "03",
-
-  // Preencha com a url do seu perfil no github: (https://github.com/[user]).
-  github: "https://github.com/viniseven",
-
-  // Preencha com a url do seu perfil no linkedin: (https://www.linkedin.com/in/[user]).
-  linkedin_url: "https://www.linkedin.com/in/marcus-vinicius-525b0716a/",
-
-  // Preencha com a url do seu perfil da rockeseat: (https://app.rocketseat.com.br/me/[user]).
-  rocketseat_profile: "https://app.rocketseat.com.br/me/marcus-vinicius-1574269890",
->>>>>>>> viniseven-viniseven:users/viniseven.js
+  rocketseat_profile: 'https://app.rocketseat.com.br/me/lfoalves',
 
   // Preencha os campos abaixo com os links dos desafios que você já concluiu.
   // Preencha o campo [codigo_url] com o link do codepen ou github.
@@ -68,115 +41,101 @@ export const data = {
   // Desafios do Stage 02
   stage02: {
     fase01_corrigindo_bugs01: {
-<<<<<<<< HEAD:users/lfoalves.js
-      codigo_url: "https://github.com/lfoalves/explorer/tree/master/step2/stage2/desafios/fase01",
-      deploy_url: "https://lfoalves.github.io/explorer/step2/stage2/desafios/fase01/index.html"
+      codigo_url: 'https://github.com/lfoalves/explorer/tree/master/step2/stage2/desafios/fase01',
+      deploy_url: 'https://lfoalves.github.io/explorer/step2/stage2/desafios/fase01/index.html',
     },
     fase02_corrigindo_bugs02: {
-      codigo_url: "https://github.com/lfoalves/explorer/tree/master/step2/stage2/desafios/fase02",
-      deploy_url: "https://lfoalves.github.io/explorer/step2/stage2/desafios/fase02/index.html"
+      codigo_url: 'https://github.com/lfoalves/explorer/tree/master/step2/stage2/desafios/fase02',
+      deploy_url: 'https://lfoalves.github.io/explorer/step2/stage2/desafios/fase02/index.html',
     },
     fase02_semantica_acessibilidade: {
-      codigo_url: "https://github.com/lfoalves/explorer/tree/master/step2/stage2/desafios/acessibilidade",
-      deploy_url: "https://lfoalves.github.io/explorer/step2/stage2/desafios/acessibilidade/index.html"
+      codigo_url:
+        'https://github.com/lfoalves/explorer/tree/master/step2/stage2/desafios/acessibilidade',
+      deploy_url:
+        'https://lfoalves.github.io/explorer/step2/stage2/desafios/acessibilidade/index.html',
     },
     fase02_recriando_layout: {
-      codigo_url: "https://github.com/lfoalves/explorer/tree/master/step2/stage2/desafios/recriando_layout",
-      deploy_url: "https://lfoalves.github.io/explorer/step2/stage2/desafios/recriando_layout/index.html"
-========
-      codigo_url: "https://github.com/viniseven/explorer_challenge_correct_bugs02",
-      deploy_url: "https://viniseven.github.io/explorer_challenge_correct_bugs01/"
+      codigo_url:
+        'https://github.com/lfoalves/explorer/tree/master/step2/stage2/desafios/recriando_layout',
+      deploy_url:
+        'https://lfoalves.github.io/explorer/step2/stage2/desafios/recriando_layout/index.html',
     },
-    fase02_corrigindo_bugs02: {
-      codigo_url: "https://github.com/viniseven/explorer_challenge_correct_bugs02",
-      deploy_url: "https://viniseven.github.io/explorer_challenge_correct_bugs02/"
-    },
-    fase02_semantica_acessibilidade: {
-      codigo_url: "https://github.com/viniseven/explorer_challenge_semantics_and_accessibility",
-      deploy_url: "https://viniseven.github.io/explorer_challenge_semantics_and_accessibility/"
-    },
-    fase02_recriando_layout: {
-      codigo_url: "https://github.com/viniseven/explorer_challenge_recreating-_layout",
-      deploy_url: "https://viniseven.github.io/explorer_challenge_recreating-_layout/"
->>>>>>>> viniseven-viniseven:users/viniseven.js
-    }
   },
 
   // Desafios do Stage 03
   stage03: {
     criando_formularios: {
-      codigo_url: "https://github.com/viniseven/explorer_creating_form",
-      deploy_url: "https://viniseven.github.io/explorer_creating_form/"
+      codigo_url: '',
+      deploy_url: '',
     },
     mobile_first: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     grid_com_animacoes: {
-      codigo_url: "https://github.com/viniseven/explorer_creating_grid_layout",
-      deploy_url: "https://viniseven.github.io/explorer_creating_grid_layout/"
-    }
+      codigo_url: '',
+      deploy_url: '',
+    },
   },
 
   // Desafios do Stage 04
   stage04: {
     exercicio01: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     exercicio02: {
-      codigo_url: "",
-      deploy_url: ""
-    }
+      codigo_url: '',
+      deploy_url: '',
+    },
   },
 
   // Desafios do Stage 05
   stage05: {
     focustimer: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     focustimer_dark_mode: {
-      codigo_url: "",
-      deploy_url: ""
-    }
+      codigo_url: '',
+      deploy_url: '',
+    },
   },
 
   // Desafios do Stage 06
   stage06: {
     spa_universe: {
-      codigo_url: "",
-      deploy_url: ""
+      codigo_url: '',
+      deploy_url: '',
     },
     gitfav: {
-      codigo_url: "",
-      deploy_url: ""
-    }
+      codigo_url: '',
+      deploy_url: '',
+    },
   },
-
 
   // Desafios do Stage 07
   stage07: {
     git_push: {
-      codigo_url: ""
-    }
+      codigo_url: '',
+    },
   },
 
   // O projeto Rocket Movies será desenvolvido nos Stages 08, 09, 10, 11
   stages_08_09_10_11: {
     rocket_movies: {
-      github_frontend: "",
-      github_backend: "",
-      deploy_url: ""
-    }
+      github_frontend: '',
+      github_backend: '',
+      deploy_url: '',
+    },
   },
 
   // Projeto final
   desafio_final: {
     food_explorer: {
-      github_frontend: "",
-      github_backend: "",
-      deploy_url: ""
-    }
-  }
-}
+      github_frontend: '',
+      github_backend: '',
+      deploy_url: '',
+    },
+  },
+};
