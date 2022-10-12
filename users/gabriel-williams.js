796378df5ddb4d9da57af87431075663bc7dc5fd
gabriel-williams <gabrielwilliamcps@gmail.com>
2022-10-12 17:02:01 -0300

    feat: Adicionando exercicios do stage 4

--- users/gabriel-williams.js
+++ users/gabriel-williams.js
@@ -76,12 +76,12 @@ export const data = {
   // Desafios do Stage 04
   stage04: {
     exercicio01: {
-      codigo_url: '',
-      deploy_url: '',
+      codigo_url: 'https://codepen.io/gabriel-williams/pen/ExQeVea',
+      deploy_url: 'https://codepen.io/gabriel-williams/pen/ExQeVea',
     },
     exercicio02: {
-      codigo_url: '',
-      deploy_url: '',
+      codigo_url: 'https://codepen.io/gabriel-williams/pen/XWZPmQJ',
+      deploy_url: 'https://codepen.io/gabriel-williams/pen/XWZPmQJ',
     },
   },
 
