b199d6880e384b8e8074e816affe8441253315db
gleicebsouza <gleice_bsouza@hotmail.com>
2022-10-17 14:18:09 -0300

    add info stage3

--- users/gleicebsouza.js
+++ users/gleicebsouza.js
@@ -60,16 +60,16 @@ export const data = {
   // Desafios do Stage 03
   stage03: {
     criando_formularios: {
-      codigo_url: '',
+      codigo_url: 'https://codepen.io/gleicebsouza/pen/rNdxrZW',
       deploy_url: '',
     },
     mobile_first: {
-      codigo_url: '',
-      deploy_url: '',
+      codigo_url: 'https://github.com/gleicebsouza/grid-animacoes-mobile.git',
+      deploy_url: 'https://grid-animacoes-mobile-sdvn.vercel.app/',
     },
     grid_com_animacoes: {
-      codigo_url: '',
-      deploy_url: '',
+      codigo_url: 'https://github.com/gleicebsouza/grid-animacoes-mobile.git',
+      deploy_url: 'https://grid-animacoes-mobile-sdvn.vercel.app/',
     },
   },
 
