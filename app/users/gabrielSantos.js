efee9083b5ad7556a66adaf8c5359375c6df5161
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-10-16 16:59:07 -0300

    feat: add gabriel user infomation

---
+++ app/users/gabrielSantos.js
@@ -0,0 +1,136 @@
+export const data = {
+    // LEIA OS COMENTÁRIOS COM ATENÇÃO.
+    // QUALQUER DÚVIDA ENTRE EM CONTATO COM [Valdemir Filho] NO DISCORD DO EXPLORER
+
+    // Preencha os campos a seguir.
+    // Os valores devem estar dentro das aspas duplas (Ex: "Aqui dentro")
+
+    // Preencha com o seu nome.
+    name: "Gabriel Santos",
+
+    // Preencha com o seu e-mail.
+    email: "gabriel_santos.dev@outlook.com.br",
+
+    // Preencha com a cidade onde você mora.
+    city: "Canhotinho",
+
+    // Preencha com a sigla do estado onde você mora.
+    // Ex: SP
+    state: "PE",
+
+    // Preencha com o país onde mora.
+    country: "Brasil",
+
+    // Preencha de qual turma do Explorer você é: Ex (turma: "01").
+    turma: "06",
+
+    // Preencha com a url do seu perfil no github: (https://github.com/[user]).
+    github: "https://github.com/gabrielSantos1101?tab=repositories",
+
+    // Preencha com a url do seu perfil no linkedin: (https://www.linkedin.com/in/[user]).
+    linkedin_url: "https://www.linkedin.com/in/developer-gabriel-santos/",
+
+    // Preencha com a url do seu perfil da rockeseat: (https://app.rocketseat.com.br/me/[user]).
+    rocketseat_profile: "https://app.rocketseat.com.br/me/gabriel-santos",
+
+    // Preencha os campos abaixo com os links dos desafios que você já concluiu.
+    // Preencha o campo [codigo_url] com o link do codepen ou github.
+    // Preencha o campo [deploy_url] caso tenha feito o deploy do seu projeto: Ex: Github Pages, Netlify, Vercel.
+
+    // Desafios do Stage 02
+    stage02: {
+        fase01_corrigindo_bugs01: {
+            codigo_url: "https://github.com/gabrielSantos1101/simple-projects/tree/main/Projeto%201",
+            deploy_url: "",
+        },
+        fase02_corrigindo_bugs02: {
+            codigo_url: "https://github.com/gabrielSantos1101/simple-projects/tree/main/Projeto%202",
+            deploy_url: "",
+        },
+        fase02_semantica_acessibilidade: {
+            codigo_url: "https://github.com/gabrielSantos1101/simple-projects/tree/main/Projeto%204",
+            deploy_url: "",
+        },
+        fase02_recriando_layout: {
+            codigo_url: "https://github.com/gabrielSantos1101/simple-projects/tree/main/Rocket%20secret",
+            deploy_url: "",
+        },
+    },
+
+    // Desafios do Stage 03
+    stage03: {
+        criando_formularios: {
+            codigo_url: "https://github.com/gabrielSantos1101/Form-template-2",
+            deploy_url: "https://gabrielsantos1101.github.io/Form-template-2/",
+        },
+        mobile_first: {
+            codigo_url: "",
+            deploy_url: "",
+        },
+        grid_com_animacoes: {
+            codigo_url: "https://github.com/gabrielSantos1101/Space-cream",
+            deploy_url: "https://gabrielsantos1101.github.io/Space-cream-Grid/",
+        },
+    },
+
+    // Desafios do Stage 04
+    stage04: {
+        exercicio01: {
+            codigo_url: "",
+            deploy_url: "",
+        },
+        exercicio02: {
+            codigo_url: "",
+            deploy_url: "",
+        },
+    },
+
+    // Desafios do Stage 05
+    stage05: {
+        focustimer: {
+            codigo_url: "https://github.com/gabrielSantos1101/simple-projects/tree/main/simple%20Focus%20timer",
+            deploy_url: "",
+        },
+        focustimer_dark_mode: {
+            codigo_url: "https://github.com/gabrielSantos1101/Focus-timer-2.0",
+            deploy_url: "https://focus-timer-six.vercel.app/",
+        },
+    },
+
+    // Desafios do Stage 06
+    stage06: {
+        spa_universe: {
+            codigo_url: "https://github.com/gabrielSantos1101/Explorer-universe",
+            deploy_url: "https://explorer-universe.netlify.app/",
+        },
+        gitfav: {
+            codigo_url: "https://github.com/gabrielSantos1101/Gitfav",
+            deploy_url: "https://gabs-git-fav.vercel.app/",
+        },
+    },
+
+    // Desafios do Stage 07
+    stage07: {
+        git_push: {
+            codigo_url: "",
+        },
+    },
+
+    // O projeto Rocket Movies será desenvolvido nos Stages 08, 09, 10, 11
+    stages_08_09_10_11: {
+        rocket_movies: {
+            github_frontend: "https://github.com/gabrielSantos1101/Front-star-Movies",
+            github_backend: "https://github.com/gabrielSantos1101/api-star-movie",
+            deploy_url: "https://web-star-movies.vercel.app/",
+        },
+    },
+
+    // Projeto final
+    desafio_final: {
+        food_explorer: {
+            github_frontend: "https://github.com/gabrielSantos1101/Front-foodExplorer",
+            github_backend: "https://github.com/gabrielSantos1101/backEnd-FoodExplorer",
+            deploy_url: "https://delivery-explorer.vercel.app/",
+        },
+    },
+};
