21478629a24e1de7f8b7f0933d7014388204a67a
Valdemir Filho <filhux@gmail.com>
2024-09-02 20:05:46 -0300

    start new project

--- app/utils/getGithubProfileImg.js
+++
@@ -1,6 +0,0 @@
-function getGithubProfileImg(githubURL) {
-  const githubURLdata = new URL(githubURL);
-  return `${githubURLdata.origin}${githubURLdata.pathname}.png`;
-}
-
-export { getGithubProfileImg };
